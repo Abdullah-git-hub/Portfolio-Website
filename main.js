1f8197f16a8d97b170287fafd2a2d09d53baf019
Abdullah-git-hub <arafat200423@gmail.com>
2024-05-28 22:24:42 +0600

    finally solved the animation issues

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $(".serviceCard").hover(
+        function () {
+            $(this).addClass("activeServiceCard");
+            $(".serviceCard").not(this).removeClass("activeServiceCard");
+            var topValue = $(".activeServiceCard").position().top;
+            $(".allServicesBack").css({
+                top: topValue,
+            });
+
+            handleServicesCardCSS();
+        },
+        function () {
+            $(this).removeClass("activeServiceCard");
+            document
+                .getElementsByClassName("serviceCard")[0]
+                .classList.add("activeServiceCard");
+            var topValue = $(".activeServiceCard").position().top;
+            $(".allServicesBack").css({
+                top: topValue,
+            });
+            handleServicesCardCSS();
+        }
+    );
+});
+
+handleServicesCardCSS();
+
+$(window).on("scroll", function () {
+    document.querySelectorAll(".odometer")[0].innerHTML = 7;
+    document.querySelectorAll(".odometer")[1].innerHTML = 30;
+    document.querySelectorAll(".odometer")[2].innerHTML = 1.2;
+    document.querySelectorAll(".odometer")[3].innerHTML = 52;
+});
+
+function handleServicesCardCSS() {
+    const allServiceCards = document.getElementsByClassName("serviceCard");
+
+    for (i = 0; i < allServiceCards.length; i++) {
+        if (allServiceCards[i].classList.contains("activeServiceCard")) {
+            allServiceCards[i].querySelector("h2").style.color = "white";
+            allServiceCards[i].querySelector("i").style.transform =
+                "rotate(0deg)";
+        } else {
+            allServiceCards[i].querySelector("h2").style.color =
+                "var(--theme-primary)";
+            allServiceCards[i].querySelector("i").style.transform =
+                "rotate(90deg)";
+        }
+    }
+}
